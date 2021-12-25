7ce2cee49065bc3bd64166a4494c0a3189c57fc1
Luu Chan Thinh <chanthinhluu@gmail.com>
2021-12-25 12:12:41 +0700

    Xu ly Data va Price

--- main.js
+++ main.js
@@ -4,11 +4,37 @@ var vueInstance = new Vue({
         title: 'Áo thun nam thể thao hàng VNXK vải dày mịn - Vải Đốm',
         url: 'https://shopee.vn/%C3%81o-Thun-Xanh-D%C6%B0%C6%A1ng-gi%C3%A1-R%E1%BA%BB-D%C3%A0nh-Cho-c%E1%BA%A3-nh%C3%A0-%C4%90i-Ch%C6%A1i-i.118127317.2758818810',
         target: '_blank',
-        price: 10000,
+        price: 20000,
+        sale: 0.3,
+        selectedProduct: 0,
+        listProductDetail: [
+            {
+                image: './images/blue.jpg',
+                quantity: 8,
+                textColor: 'Màu Xanh'
+            },
+            {
+                image: './images/black.jpg',
+                quantity: 2,
+                textColor: 'Màu Đen'
+            },
+            {
+                image: './images/red.jpg',
+                quantity: 0,
+                textColor: 'Màu Đỏ'
+            }
+        ]
     },
     methods: {
-        formatPrice() {
-            const number = this.price
+
+    },
+    computed: {
+        formatOriginalPrice() {
+            var number = this.price
+            return new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'VND' }).format(number)
+        },
+        formatFinalPrice() {
+            var number = this.price - this.price * this.sale
             return new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'VND' }).format(number)
         }
     }
